700d819824536c66cc429e8e387d7cf312f0da83
John Johnson-Rodgers <john@johnthe.dev>
2019-11-11 13:21:36 -0700

    Boiler plate for index.php, .gitignore

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8"/>
+	<!-- link to Javascript-->
+
+	<!-- title-->
+	<title>Literary Mutilator</title>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
